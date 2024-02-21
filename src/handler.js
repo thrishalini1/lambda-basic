13f018b43d0fc56b99150361cc31c40d79003589
thrishalini1 <trishalini973@gmail.com>
2024-02-21 14:26:13 +0530

    sm

--- src/handler.js
+++ src/handler.js
@@ -1,18 +1,53 @@
+// 'use strict';
+
+// module.exports.hello = async (event) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: 'Go Serverless v1.0! Your function executed successfully!',
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+//   };
+
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+// };
+
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.getAllImages = async (event) => {
+  try {
+    const s3Params = {
+      Bucket: 'testbucketfortgm',
+      Prefix: '/', 
+    };
+    const s3Response = await s3.listObjectsV2(s3Params).promise();
+
+  
+    const imageUrls = s3Response.Contents.map((object) => {
+      return `https://${s3Params.Bucket}.s3.amazonaws.com/${object.Key}`;
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Images retrieved successfully',
+        images: imageUrls,
+      }),
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error retrieving images from S3' }),
+    };
+  }
 };
+
